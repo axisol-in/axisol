b6d0d8efe5d2b206d077eef72f7fbbe59f180f50
Aaditya Jauhari <aadityajauhari01@gmail.com>
2025-09-10 11:17:01 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: "class", // ✅ important for dark mode toggle
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
