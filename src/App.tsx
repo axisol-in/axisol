dd6b675c60b2386d1884f218907583e73e267839
ProblemShooter <aadityajauhari01@gmail.com>
2025-09-11 12:14:55 +0530

    changed the app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 
 import Header from "./components/Header";
 import Chatbot from "./components/Chatbot";
 
-// import HeroSection from "./components/HeroSection";
-// import WhyChooseSection from "./components/WhyChooseSection";
-// import ProjectsSection from "./components/ProjectsSection";
-// import ServiceLocationsSection from "./components/ServiceLocationsSection";
-// import VideoSection from "./components/VideoSection";
-// import UserStoriesSection from "./components/UserStoriesSection";
-// import BlogsSection from "./components/BlogsSection";
+import ContactSection from "./components/ContactSection";
 
 import { ThemeProvider } from "./context/ThemeContext";
 
@@ -20,9 +14,27 @@ import ProjectsPage from "./pages/Projects";
 import ContactPage from "./pages/Contact";
 import BlogsPage from "./pages/Blogs";
 
+const ScrollToTop: React.FC = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }, 50);
+
+    return () => clearTimeout(timer);
+  }, [pathname]);
+
+  return null;
+};
+
 const AppContent: React.FC = () => {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <div className="min-h-screen bg-white dark:bg-gray-900 text-gray-900 dark:text-white transition-colors duration-300">
         <Header />
         <Routes>
@@ -33,6 +45,7 @@ const AppContent: React.FC = () => {
           <Route path="/blogs" element={<BlogsPage />} />
         </Routes>
         <Chatbot />
+        <ContactSection />
       </div>
     </BrowserRouter>
   );
