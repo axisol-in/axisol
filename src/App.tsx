5971c6db009ae7645f2fb106ec1b6d486956a46d
Ritesh Gharat <riteshgharat05@gmail.com>
2025-09-12 12:36:14 +0530

    add home image

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Chatbot from "./components/Chatbot";
 
-// import HeroSection from "./components/HeroSection";
-// import WhyChooseSection from "./components/WhyChooseSection";
-// import ProjectsSection from "./components/ProjectsSection";
-// import ServiceLocationsSection from "./components/ServiceLocationsSection";
-// import VideoSection from "./components/VideoSection";
-// import UserStoriesSection from "./components/UserStoriesSection";
-// import BlogsSection from "./components/BlogsSection";
-
 import { ThemeProvider } from "./context/ThemeContext";
 
 import HomePage from "./pages/Home";
@@ -23,7 +15,7 @@ import BlogsPage from "./pages/Blogs";
 const AppContent: React.FC = () => {
   return (
     <BrowserRouter>
-      <div className="min-h-screen bg-white dark:bg-gray-900 text-gray-900 dark:text-white transition-colors duration-300">
+      <div className="min-h-screen bg-white text-gray-900 dark:text-white transition-colors duration-300">
         <div className="flex justify-center">
           <Header />
         </div>
